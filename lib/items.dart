6e5c5fcbc95f80b86a0a86a3435297ab24b271e4
kien-to <ktto@umass.edu>
2023-02-12 17:24:15 -0500

    init

---
+++ lib/items.dart
@@ -0,0 +1,20 @@
+List items = [
+  {
+    "header": "Learn",
+    "description":
+        "Online chat which provides its users maximum functionality to simplify the search",
+    "image": "assets/images/1.png"
+  },
+  {
+    "header": "Build",
+    "description":
+        "Easy stuff to learn and build your own app with flutter",
+    "image": "assets/images/2.png"
+  },
+  {
+    "header": "Launch",
+    "description":
+        "Chat app with flutter is easy to launch in android and ios",
+    "image": "assets/images/3.png"
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,322 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:namer_app/items.dart';
+import 'package:namer_app/pages/SignUpPage.dart';
+import 'package:namer_app/widget/BigCard.dart';
+import 'package:provider/provider.dart';
+
+import 'animation/ScaleRoute.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome Screen',
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget {
+  @override
+  _WelcomeScreen createState() => _WelcomeScreen();
+}
+
+class _WelcomeScreen extends State<WelcomeScreen> {
+  List<Widget> slides = items
+      .map((item) => Container(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
+          child: Column(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: 
+                Image.asset(
+                  item['image'],
+                  fit: BoxFit.fitWidth,
+                  width: 220.0,
+                  alignment: Alignment.bottomCenter,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                fit: FlexFit.tight,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Column(
+                    children: <Widget>[
+                      Text(item['header'],
+                          style: TextStyle(
+                              fontSize: 50.0,
+                              fontWeight: FontWeight.w300,
+                              color: Color(0XFF3F3D56),
+                              height: 2.0)),
+                      Text(
+                        item['description'],
+                        style: TextStyle(
+                            color: Colors.grey,
+                            letterSpacing: 1.2,
+                            fontSize: 16.0,
+                            height: 1.3),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )))
+      .toList();
+
+  List<Widget> indicator() => List<Widget>.generate(
+      slides.length,
+      (index) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 3.0),
+            height: 10.0,
+            width: 10.0,
+            decoration: BoxDecoration(
+                color: currentPage.round() == index
+                    ? Color(0XFF256075)
+                    : Color(0XFF256075).withOpacity(0.2),
+                borderRadius: BorderRadius.circular(10.0)),
+          ));
+
+  double currentPage = 0.0;
+  final _pageViewController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    // print('kien');
+
+    return Scaffold(
+      
+      body: Container(
+        child: Stack(
+          children: <Widget>[
+            PageView.builder(
+              controller: _pageViewController,
+              itemCount: slides.length,
+              itemBuilder: (BuildContext context, int index) {
+                _pageViewController.addListener(() {
+                  setState(() {
+                    currentPage = _pageViewController.page!;
+                  });
+                });
+                return slides[index];
+              },
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(top: 70.0),
+                  padding: EdgeInsets.symmetric(vertical: 40.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: indicator(),
+                  ),
+                )),
+                Positioned(
+              top: 60.0,
+              right: 40.0,
+              child: InkWell(
+                onTap: () {
+                  // Add your code for when the text is pressed
+                  print("pressed");
+                  Navigator.push(context, ScaleRoute(page: SignUpPage())
+                      // MaterialPageRoute(builder: (context) => SignUpPage()),
+                      );
+                },
+                child: IgnorePointer(
+                  child: Text('skip',
+                      style: TextStyle(
+                          color: Color(0XFF256075),
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w300)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return ChangeNotifierProvider(
+//       create: (context) => MyAppState(),
+//       child: MaterialApp(
+//         title: 'Namer App',
+//         theme: ThemeData(
+//           useMaterial3: true,
+//           colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+//         ),
+//         home: MyHomePage(),
+//       ),
+//     );
+//   }
+// }
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+
+  // ↓ Add this.
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = GeneratorPage();
+        break;
+      case 1:
+        page = FavoritesPage();
+        break;
+      default:
+        throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  // ↓ Replace print with this.
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavorite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text('You have '
+              '${appState.favorites.length} favorites:'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
