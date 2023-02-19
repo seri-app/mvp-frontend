defedeabf6c164aad733c74cea8f146b4fc572e3
kien-to <ktto@umass.edu>
2023-02-18 19:37:35 -0500

    add global theme template

--- lib/src/shared/app.dart
+++ lib/src/shared/app.dart
@@ -6,10 +6,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        fontFamily: 'Roboto',
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.purple),
+          bodyLarge: TextStyle(
+              fontSize: 72.0, fontWeight: FontWeight.bold, color: Colors.green),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Welcome Screen',
       home: WelcomeScreen(),
     );
   }
 }
-
